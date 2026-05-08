c88b4777ab40d12986e6109fad86aaeadc090a3c
Usman Nazir <usman.nazir@guardian.co.uk>
2026-05-08 23:36:38 +0100

    Improvements

--- Sources/LogBook.swift
+++ Sources/LogBook.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  Qalam
-//
-//  Created by Usman_Nazir on 08/05/2026.
-//
-
 import Foundation
 
 typealias LogRepresentable = (String, String)
@@ -15,16 +8,28 @@ class LogBook {
     
     private var logs: [LogRepresentable] = []
     private var subsystems: Set<String> = []
+    private var logCount: Int = 15
     
     private init() {}
     
     func insert(_ message: Any, subsystem: LogSubsystem) {
-        
         guard let stringMessage = message as? String else { return }
+        
+        let subsystemName = subsystem.subsystemName
+        let subsystemLogs = logs.filter { $0.1 == subsystemName }
+        if subsystemLogs.count >= logCount {
+            if let index = logs.firstIndex(where: { $0.1 == subsystemName }) {
+                logs.remove(at: index)
+            }
+        }
+        
+        logs.append((stringMessage, subsystemName))
         subsystems.insert(subsystem.subsystemName)
-        logs.append((stringMessage, subsystem.subsystemName))
-        print("insert \(message) for subs \(subsystem)")
         print(subsystems)
         print(logs)
+    }
+    
+    func setLogCount(_ count: Int) {
+        logCount = count
     }
 }
